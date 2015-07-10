af49ade4955f6bfbddf6f4b2fb9c933c34a51982
bigbanton <c.banton12@gmail.com>
2015-07-09 22:53:54 -0400

    added change task custom action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,6 +10,13 @@ class TasksController < ApplicationController
   respond_with(@tasks)
 end
 
+def change
+  @task.update_attributes(state: params[:state])
+  respond_to do |format|
+    format.html {redirect_to tasks_path, notice: "Task Update"}
+  end
+  
+
   # GET /tasks/1
   # GET /tasks/1.json
   def show
