f974986b9da930842103d115b06c92db46b99118
bigbanton <c.banton12@gmail.com>
2015-07-09 22:49:17 -0400

    added state to the tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,9 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-  @tasks = current_user.tasks
+  @to_do = current_user.tasks.where(state: "to_do")
+  @doing = current_user.tasks.where(state: "doing")
+  @done = current_user.tasks.where(state: "done")
   respond_with(@tasks)
 end
 
@@ -62,6 +64,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content)
-    end
+  params.require(:task).permit(:content,:state)
+end
 end
